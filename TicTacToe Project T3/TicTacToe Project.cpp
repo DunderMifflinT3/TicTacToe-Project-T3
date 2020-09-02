3575f3aa3a0c364e8b19cd8070a5cf379eb997fb
Danny Veloso <dundermifflint3@gmail.com>
2020-09-02 11:45:40 -0400

    Added input function

--- TicTacToe Project T3/TicTacToe Project.cpp
+++ TicTacToe Project T3/TicTacToe Project.cpp
@@ -6,7 +6,7 @@ void get_input(int[]);
 int main()
 {
 	//1D array 0-8 (ensure user input - 1 to match array) -DV
-	int arrayBoard[9];
+	int arrayBoard[9] = { 0, 1, 2, 3, 4, 5, 6, 7, 8};
 
 	//call user input function -DV
 	get_input(arrayBoard);
@@ -22,12 +22,21 @@ void get_input(int[])
 	for (int i = 1; i <= 9; i++)
 	{
 
-		cout << "Player 1 pick a square by typing a number 1 - 9";
+		cout << "Player 1 pick a square by typing a number 1 - 9" << endl;
 		cin >> playerinput;
-
+		
+		//input validation -DV
+		//need to include double selection error -DV
+		if (playerinput > 9 || playerinput < 1)
+		{
+			cout << "Invaild Number" << endl;
+		}
 		//include AI function for player2 input -DV
-
-		showBoard((playerinput - 1), playerAI);
+		//rename showBoard and playerAI to appropriate function and variable -DV
+		else
+		{
+			showBoard((playerinput - 1), playerAI);
+		}
 	}
 
 	return(playerinput, playerAI);
