ca520d4857beae34f4eeceb614b859da57927121
Danny Veloso <dundermifflint3@gmail.com>
2020-09-02 11:05:13 -0400

    Added 1D array and input function

--- TicTacToe Project T3/TicTacToe Project.cpp
+++ TicTacToe Project T3/TicTacToe Project.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	
+void get_input(int[]);
+
+int main()
+{
+	//1D array 0-8 (ensure user input - 1 to match array) -DV
+	int arrayBoard[9];
+
+	//call user input function -DV
+	get_input(arrayBoard);
+
+	return 0;
+}
+
+//Player input function -DV
+void get_input(int[])
+{
+	int playerinput;
+
+	for (int i = 1; i <= 9; i++)
+	{
+
+		cout << "Player 1 pick a square by typing a number 1 - 9";
+		cin >> playerinput;
+
+		//include AI function for player2 input -DV
+
+		showBoard((playerinput - 1), playerAI);
+	}
+
+	return(playerinput, playerAI);
 }
