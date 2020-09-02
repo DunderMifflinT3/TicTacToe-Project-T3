f43f2fc3a3812951edb04dc4529ec5fe6d9adb3c
Michael LaNeve <mlaneve2000@gmail.com>
2020-09-02 13:22:49 -0400

    Added drawBoard function that pushes in values from int arrayBoard[] and displays them in a 3 x 3 grid.

--- TicTacToe Project T3/TicTacToe Project.cpp
+++ TicTacToe Project T3/TicTacToe Project.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 
 void player_input(int[]);
+void drawBoard(int[]);
 
 int main()
 {
 	//1D array 0-8 (ensure user input - 1 to match array) -DV
 	int arrayBoard[9] = { 0, 1, 2, 3, 4, 5, 6, 7, 8};
+
+	//Draws the board with the values in arrayBoard
+	drawBoard(arrayBoard);
 
 	//call user input function -DV
 	player_input(arrayBoard);
@@ -40,4 +44,18 @@ void player_input(int[])
 	}
 
 	return(playerinput, playerAI);
+}
+
+//Draws a 3 x 3 grid and displays the corresponding input value in each square
+void drawBoard(int input[])
+{
+	cout << "     |     |     " << endl;
+	cout << "  " << input[0] << "  |  " << input[1] << "  |  " << input[2] << "  " << endl;		//First row inputs
+	cout << "_____|_____|_____" << endl;
+	cout << "     |     |     " << endl;
+	cout << "  " << input[3] << "  |  " << input[4] << "  |  " << input[5] << "  " << endl;		//Second row inputs
+	cout << "_____|_____|_____" << endl;
+	cout << "     |     |     " << endl;
+	cout << "  " << input[6] << "  |  " << input[7] << "  |  " << input[8] << "  " << endl;		//Third row inputs
+	cout << "     |     |     " << endl;
 }
