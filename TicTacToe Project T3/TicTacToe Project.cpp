75db8d54ed181da7fe4b7e910a4da0905dcd3cea
DunderMifflinT3 <70239101+DunderMifflinT3@users.noreply.github.com>
2020-09-13 18:12:51 -0400

    Update TicTacToe Project.cpp

--- TicTacToe Project T3/TicTacToe Project.cpp
+++ TicTacToe Project T3/TicTacToe Project.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iostream>
+#include <string>
 #include <time.h> 
 #include <stdlib.h>   
 #include <stdio.h> 
@@ -15,11 +16,9 @@ bool checkTTT_BoardX();
 void checkTie();
 int moveCheck(char);
 int moveCheckAI(int);
-void ClearScreen();
+//void ClearScreen();
 int main()
 {
-	char TTT_Board[9] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
-
 	//Calls Help
 	callHelp();
 
@@ -35,7 +34,7 @@ int playerAI()
 {
 	int randNum;
 	srand(time(NULL));
-	randNum = rand() % 10;// Returns random numer from 0-8
+	randNum = rand() % 9;// Returns random numer from 0-8
 	randNum = randNum;
 	return randNum;
 }
@@ -50,14 +49,14 @@ void player_input(char TTT_Board[])
 	{
 		cout << "Player 1 pick a square by typing a number 1 - 9" << endl;
 		cin >> playerinput;
-		cin.ignore(10, '\n');
+		cin.ignore(numeric_limits <streamsize> ::max(), '\n');
 
 		while (playerinput != '1' && playerinput != '2' && playerinput != '3' && playerinput != '4' && playerinput != '5' && playerinput != '6' && playerinput != '7' && playerinput != '8'
 			&& playerinput != '9' && playerinput != 'h' && playerinput != 'H' && playerinput != 'r' && playerinput != 'R')
 		{
 			cout << "Invalid Input" << endl;
 			cin >> playerinput;
-			cin.ignore(10, '\n');
+			cin.ignore(numeric_limits <streamsize> ::max(), '\n');
 		}
 
 		//Move Check prevent double move
@@ -70,7 +69,7 @@ void player_input(char TTT_Board[])
 		{
 			cout << "Invalid move, please pick again" << endl; // input val for player 
 			cin >> playerinput;
-			cin.ignore(10, '\n');
+			cin.ignore(numeric_limits <streamsize> ::max(), '\n');
 		}
 		//char AIhold =static_cast<char>(playerAI());
 
@@ -320,6 +319,8 @@ void callHelp()
 
 
 	cout << "Input a number between 1 and 9, as shown below, to place an X (Player 1) or an O (Player 2) in that spot." << endl;
+	cout << "Only first number will count as an input, all others will be ignored." << endl;
+	cout << "Cannot choose a box that already has a X or an O" << endl;
 	cout << "     |     |     " << endl;
 	cout << "  1  |  2  |  3  " << endl;		//First row inputs
 	cout << "_____|_____|_____" << endl;
@@ -399,9 +400,8 @@ void playAgain()
 		{
 			TTT_Board[i] = ' ';
 		}
-		//drawBoard(TTT_Board);
-		//player_input(TTT_Board);
-		main();
+		drawBoard(TTT_Board);
+		player_input(TTT_Board);
 	}
 	else if (input == 'N' || input == 'n')
 	{
